5ef35cf971a7198dfd352c87f1e51a9ec28e7045
bedosebestyen <bedosebestyen@gmail.com>
2023-08-29 15:54:37 +0200

    ICMP, TCP logging

--- __main__.py
+++ __main__.py
@@ -1,21 +1,42 @@
 import asyncio
-import os
-import sys
-import time
-from chcker import check_tcp_syn, check_icmp
+from chcker import check_tcp, check_icmp
 import logging
 
 iplistdir = "/run/known-webservers-for-connectivity-test/latest"
-logging.basisConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG, filename="main.log")
 
+# Logger for exceptions
+exception_logger = logging.getLogger("exceptions")
+exception_logger.setLevel(logging.DEBUG)
+exception_handler = logging.FileHandler("exceptions.log")
+exception_logger.addHandler(exception_handler)
 
+# Logger for info messages
+info_logger = logging.getLogger("info")
+info_logger.setLevel(logging.INFO)
+info_handler = logging.FileHandler("info.log")
+info_logger.addHandler(info_handler)
+
+def print_logs(*loggers):
+    for logger in loggers:
+        for handler in logger.handlers:
+            with open(handler.baseFilename, "r") as file:
+                print(f"Log file: {handler.baseFilename}")
+                print(file.read())
+                print("=" * 80)
+    
 async def main():
-    await check_tcp_syn("google.com", 80, 2)
+    await check_tcp("google.com", 80, 2)
     await check_icmp("google.com", 2)
+    print_logs(exception_logger, info_logger)
+    
     
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        print(f'Exception: {e}')
     
         
     
--- chcker.py
+++ chcker.py
@@ -1,62 +1,67 @@
+import aioping
 import socket
 import asyncio
 import logging
 
+
 logging.basicConfig(level=logging.INFO)
 
-async def check_tcp_syn(host, port, timeout=1):
-    try:        
+async def check_tcp(host, port, timeout=1):
+    try:
+        """
         try:
             socket.gethostbyname(host)
         except socket.gaierror as e:
             raise ValueError(f"Invalid IP address or hostname: {host}")
-        
+        """
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            """if mark is not None:
-                sock.setsockopt(socket.SOL_SOCKET, socket.SO_MARK, mark)"""
+            """
+            if mark is not None:
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_MARK, mark)
+            """
             try:
-                #_, _ placeholder for reader, writer
-                _, _ = await asyncio.wait_for(asyncio.open_connection(host, port), timeout)
-                print("siker")
+                _, _ = await asyncio.wait_for(asyncio.open_connection(host, port), timeout=timeout)
+                logging.info(f"{host} is reachable TCP")
+                return True
             except asyncio.TimeoutError as e:
-                 logging.error(f"Connection to {host}:{port} timed out: {e}")
-                 return(f"Connection to {host}:{port} timed out: {e}")
+                 logging.exception(f"Connection to {host}:{port} timed out: {e}")
+                 raise
     except OSError as e:
-        logging.error(f"Socket error: {e}")
-        return(f"Socket error: {e}")
+        logging.exception(f"Socket error: {e}")
+        raise
     
-
-async def send_icmp_echo_request(host, timeout):
+async def check_icmp(host, timeout=2):
     """
-    Send an ICMP echo request to the specified host.
+    Sends an ICMP echo request to the specified host.
 
     Args:
-        host (str): The host to send the ICMP echo request to.
-        timeout (float): The timeout value in seconds.
+            host (str): The IP address or hostname of the target host.
+            weight (int): The number of echo requests to send.
+            timeout (float, optional): The maximum time to wait for a response, in seconds. Defaults to 2.
 
-    Returns:
-        None
+        Returns:
+            bool: True if the host responded, False otherwise.
+
+        Raises:
+            asyncio.TimeoutError: If the ping times out.
+            Exception: If an error occurs while pinging the host.
     """
     try:
-        reader, writer = await asyncio.open_connection(host, 1024, timeout=timeout)  # Using port 1 for ICMP
-        writer.write(b'')  # Sending an empty packet as ICMP echo request
-        await writer.drain()  # Wait until data is flushed (optional)
-        writer.close()  # Close the writer
-
-        # Receiving a response (if any)
-        data = await reader.read(1024)
-
-        if data:
-            print("ICMP echo successful")
+        delay = await aioping.ping(host, timeout=timeout)
+        if delay is not None:
+            logging.info(f"{host} is reachable ({delay} ms) ICMP")
+            return True
         else:
-            print("No ICMP response received")
-
-    except asyncio.TimeoutError as e:
-        print(f"ICMP echo to {host} timed out: {e}")
-    except OSError as e:
-        print(f"Socket error: {e}")
-    finally:
-        writer.close()
+            logging.error(f"Host {host} did not respond")
+            raise
+    except asyncio.TimeoutError:
+        logging.exception(f"Timeout while pinging {host}")
+        raise
+    except Exception as e:
+        logging.exception(f"An error occurred while pinging {host}: {e}")
+        raise
+        
+    
        
 
 
