af18fd898005a54b8b110a48e000896c61d8a47d
bedosebestyen <bedosebestyen@gmail.com>
2023-09-06 11:00:45 +0200

    ICMP,TCPcheck, logging works but needs fine tuning

--- __main__.py
+++ __main__.py
@@ -2,7 +2,13 @@ import asyncio
 from chcker import check_tcp, check_icmp
 import logging
 import sys
-iplistdir = "google.com"
+import random
+
+"""
+TODO: Test on linux, cli could be deleted, other protocols in chcker, implement some success ratio
+        make the code more readable/cleaner, implementing SO_MARK, implementing scripts for IP pool,
+        when there is an exception script pauses a little need to know why exactly, timeouts needs streamlining
+"""
 
 logging.basicConfig(level=logging.INFO, 
                     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -10,21 +16,76 @@ logging.basicConfig(level=logging.INFO,
                         logging.StreamHandler(sys.stdout),
                         logging.FileHandler("base_log", mode="w")
                     ])
-async def main() -> None:
-    try:
-        
-        await check_tcp("google.com", 80, 2)
-        await check_icmp("google.com", 2)
-    except Exception as e:
-        logging.exception(f'An exception occured during the checks: {e}')
-    
 
+def hosts_from_file(filename):
+    with open(filename, 'r') as file:
+        hosts = [line.strip() for line in file]
+    return hosts
+
+notreachable_hosts = set()
+
+hosts = hosts_from_file("source.txt")
+
+def random_host(notreachable_hosts, hosts):
+    # Take out the unreachable hosts from the hosts list, then return a random host from them.
+    reachable_hosts = [host for host in hosts if host not in notreachable_hosts]
+    #gives back the first element of a 1 long list
+    return random.sample(reachable_hosts, 1)[0]
+
+
+
+weights = {
+        check_icmp: 3,
+        check_tcp : 3
+    }
+
+
+
+
+def weighted_run(weights, task_args):
+    # Iterate through the key value pairs of the dictionary(weight=weight, test = check_icmp...)
+    # Call the function associated with each test, _ placeholder
+    # Return a list with the tasks
+    return [task_args[test]() for test, weight in weights.items() for _ in range(weight)]
+
+#needs to streamline main() too chaotic as it is
+async def main() -> None:
+    #Pick random host from reachable
+    rnd_host = random_host(notreachable_hosts, hosts)
+    
+    
+    """
+    Lambda is used here because you want to assign the function object itself
+    not the result of the function call. It creates and returns the tests.
+"""
+    task_args = {
+    check_icmp: lambda: asyncio.create_task(check_icmp(rnd_host, 2)),
+    check_tcp: lambda: asyncio.create_task(check_tcp(rnd_host, 80, 2))
+    }
+    # Create a list of tasks
+    tasks = weighted_run(weights, task_args)
+    
+    # with .as_completed as soon as a task is finished we can work with the result, .gather maybe better will test later
+    for task in asyncio.as_completed(tasks):
+        try:
+            #wait for a task to finish
+            results = await asyncio.gather(*tasks)
+            for result in results:
+                if not result:
+                    notreachable_hosts.add(rnd_host)
+        except Exception as e:
+            logging.error(f'An exception occurred during the checks: {e}')
+            notreachable_hosts.add(rnd_host)
+    logging.info(notreachable_hosts)
+        
+    
 if __name__ == '__main__':
     try:
-        asyncio.run(main())
-    except Exception as e:
-        logging.exception(f"An error occured during __main__: {e}")
-    
-        
-    
-    
+        while True:
+            try:
+                asyncio.run(main())
+            except Exception as e:
+                logging.error(f"An error occured during __main__: {e}")
+    except KeyboardInterrupt:
+        #Stop when ctrl + c or ctrl + z is pressed
+        logging.info("Exiting the program...")
--- chcker.py
+++ chcker.py
@@ -1,65 +1,31 @@
 import aioping
 import socket
+import sys
 import asyncio
 import logging
 
 
-logging.basicConfig(level=logging.INFO)
 
-async def check_tcp(host, port, timeout=1):
+async def check_tcp(host, port=80, timeout=1):
     try:
-        """
-        try:
-            socket.gethostbyname(host)
-        except socket.gaierror as e:
-            raise ValueError(f"Invalid IP address or hostname: {host}")
-        """
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            """
-            if mark is not None:
-                sock.setsockopt(socket.SOL_SOCKET, socket.SO_MARK, mark)
-            """
-            try:
-                _, _ = await asyncio.wait_for(asyncio.open_connection(host, port), timeout=timeout)
-                logging.info(f"{host} is reachable TCP")
-                return True
-            except asyncio.TimeoutError as e:
-                 logging.exception(f"Connection to {host}:{port} timed out: {e}")
-                 raise
-    except OSError as e:
-        logging.exception(f"Socket error: {e}")
-        raise
+        await asyncio.wait_for(asyncio.open_connection(host, port), timeout=timeout)
+        logging.info(f"{host} is reachable TCP")
+        return True
+    except (asyncio.TimeoutError, OSError) as e:
+             logging.error(f"Connection to {host}:{port} timed out: {e}")
+             
+        
     
 async def check_icmp(host, timeout=2):
-    """
-    Sends an ICMP echo request to the specified host.
-
-    Args:
-            host (str): The IP address or hostname of the target host.
-            weight (int): The number of echo requests to send.
-            timeout (float, optional): The maximum time to wait for a response, in seconds. Defaults to 2.
-
-        Returns:
-            bool: True if the host responded, False otherwise.
-
-        Raises:
-            asyncio.TimeoutError: If the ping times out.
-            Exception: If an error occurs while pinging the host.
-    """
     try:
         delay = await aioping.ping(host, timeout=timeout)
         if delay is not None:
             logging.info(f"{host} is reachable ({delay} ms) ICMP")
             return True
-        else:
-            logging.error(f"Host {host} did not respond")
-            raise
-    except asyncio.TimeoutError:
-        logging.exception(f"Timeout while pinging {host}")
-        raise
-    except Exception as e:
-        logging.exception(f"An error occurred while pinging {host}: {e}")
-        raise
+        
+    except (asyncio.TimeoutError, OSError) as e:
+             logging.error(f"Connection to {host} timed out: {e}")
+        
         
     
        
