5a1e43ae642b220b67845867dafda61c30c30fe4
bedosebestyen <bedosebestyen@gmail.com>
2023-09-08 15:26:36 +0200

    TCP should be able to use SO_MARK now

--- __main__.py
+++ __main__.py
@@ -3,14 +3,14 @@ from chcker import check_tcp, check_icmp
 import logging
 import sys
 import random
-
+import os
+import time
 """
 TODO: Test on linux, cli could be deleted, other protocols in chcker, implement some success ratio
         make the code more readable/cleaner, implementing SO_MARK, implementing scripts for IP pool,
         when there is an exception script pauses a little need to know why exactly, timeouts needs streamlining
 """
-
-source = "/var/lib/top-1000-domains/hungary"
+source = "/run/known-webservers-for-connectivity-test/latest"
 
 logging.basicConfig(level=logging.INFO, 
                     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -20,9 +20,12 @@ logging.basicConfig(level=logging.INFO,
                     ])
 
 def hosts_from_file(source):
-    with open(source, 'r') as file:
-        hosts = [line.strip() for line in file]
-    return hosts
+    ip_list = []
+    files = os.listdir(source)
+
+    for file in files:
+        ip_list.append(file)
+    return ip_list
 
 notreachable_hosts = set()
 
@@ -41,45 +44,44 @@ weights = {
         check_tcp : 1
     }
 
-
-
-
 def weighted_run(weights, task_args):
     # Iterate through the key value pairs of the dictionary(weight=weight, test = check_icmp...)
     # Call the function associated with each test, _ placeholder
     # Return a list with the tasks
     return [task_args[test]() for test, weight in weights.items() for _ in range(weight)]
 
+
+
 #needs to streamline main() too chaotic as it is
 async def main() -> None:
     #Pick random host from reachable
     rnd_host = random_host(notreachable_hosts, hosts)
     
-    
+           
     """
     Lambda is used here because you want to assign the function object itself
     not the result of the function call. It creates and returns the tests.
 """
     task_args = {
-    check_icmp: lambda: asyncio.create_task(check_icmp(rnd_host, 2)),
+    check_icmp: lambda: asyncio.create_task(check_icmp(rnd_host)),
     check_tcp: lambda: asyncio.create_task(check_tcp(rnd_host, 80, 2))
     }
     # Create a list of tasks
     tasks = weighted_run(weights, task_args)
     
-    # with .as_completed as soon as a task is finished we can work with the result, .gather maybe better will test later
+    # with .as_completed as soon as a task is finished we can work with the result
     for task in asyncio.as_completed(tasks):
         try:
             #wait for a task to finish
-            results = await asyncio.gather(*tasks)
-            for result in results:
-                if not result:
-                    notreachable_hosts.add(rnd_host)
+            result = await task
+            if not result:
+                notreachable_hosts.add(rnd_host)
         except Exception as e:
             logging.error(f'An exception occurred during the checks: {e}')
             notreachable_hosts.add(rnd_host)
     logging.info(notreachable_hosts)
-        
+    
+
     
 if __name__ == '__main__':
     try:
@@ -92,3 +94,5 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         #Stop when ctrl + c or ctrl + z is pressed
         logging.info("Exiting the program...")
+        
+    
--- chcker.py
+++ chcker.py
@@ -1,39 +1,38 @@
-import aioping
-import socket
-import sys
 import asyncio
 import logging
-
-
+import aioping
+import socket
 
 async def check_tcp(host, port=80, timeout=1):
+    #Get the current event loop
+    loop = asyncio.get_running_loop()
+    #Create a TCP socket object
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #Set the SO_MARK with value 10
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_MARK, 10)
+    #Make socket non-blocking
+    sock.setblocking(False)
+    #Try to connect
+
     try:
-        await asyncio.wait_for(asyncio.open_connection(host, port), timeout=timeout)
-        logging.info(f"{host} is reachable TCP")
-        return True
-    except (asyncio.TimeoutError, OSError) as e:
-             logging.error(f"Connection to {host}:{port} timed out: {e}")
+          logging.info('1')
+          await loop.sock_connect(sock, (host, port))
+          logging.info(f"{host} is reachable on {port} TCP")
+          return True
+    except Exception as e:
+          logging.error(f"Connection failed to {host}:{port} TCP: {e}")
+    finally:
+          sock.close()
              
         
     
-async def check_icmp(host, timeout=2):
+async def check_icmp(host):
     try:
-        delay = await aioping.ping(host, timeout=timeout)
-        if delay is not None:
-            logging.info(f"{host} is reachable ({delay} ms) ICMP")
+            delay = await aioping.ping(host)
+            if delay is not None:
+                  logging.info(f"{host} is reachable {delay} ms ICMP")
+                  return True
             
-            return True
-            
-
-        
-    except (asyncio.TimeoutError, OSError) as e:
-             logging.error(f"Connection to {host} timed out: {e}")
-        
-        
-    
-       
-
-
-
-
-
+    except (Exception, asyncio.TimeoutError) as e:
+          logging.error(f"Connection to {host} timed out: {e} ICMP")
+          
